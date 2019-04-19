c444893607811dd0f3ab9ec4c84a92c2dcc70190
ximua00 <kishanarendra@gmail.com>
2019-04-19 11:51:28 +0200

    dataloader working

---
+++ assignment_1/DataLoader.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+
+class DataLoader:
+    def __init__(self, english_data_path, french_data_path):
+        self.english_data = open(english_data_path, "r").readlines()
+        self.french_data = open(french_data_path, "r").readlines()
+
+    def preprocess(self, sentence):
+        output = sentence[:-2] # remove /n character
+        output = output.lower()
+        return output.split()
+    
+    def generate_sentence_pairs(self):
+        for e,f in zip(self.english_data, self.french_data):
+            e = self.preprocess(e)
+            f = self.preprocess(f)
+            f.insert(0, "NULL")
+            yield (e,f)
+
+
+if __name__ == "__main__":
+    english_data_path = "./training/hansards.36.2.e"
+    french_data_path = "./training/hansards.36.2.f"
+
+    data = DataLoader(english_data_path, french_data_path)
+    max_idx = 10
+    for idx, pair in enumerate(data.generate_sentence_pairs()):
+        if idx > max_idx:
+            break
+        print(pair)
