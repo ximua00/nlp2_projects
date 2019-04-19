e498ecc9145f41d1e36057ce2c01fde8c062c867
ximua00 <kishanarendra@gmail.com>
2019-04-19 12:55:14 +0200

    Added count vocab length

--- assignment_1/DataLoader.py
+++ assignment_1/DataLoader.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
 
-
 class DataLoader:
     def __init__(self, english_data_path, french_data_path):
         self.english_data = open(english_data_path, "r").readlines()
         self.french_data = open(french_data_path, "r").readlines()
+        self.n_english_vocab = self.vocabulary_length(self.english_data)
 
     def preprocess(self, sentence):
         output = sentence[:-2] # remove /n character
@@ -18,14 +18,23 @@ class DataLoader:
             f.insert(0, "NULL")
             yield (e,f)
 
+    def vocabulary_length(self, languange_data):
+        vocab = defaultdict(int)
+        for sentence in languange_data:
+            for word in self.preprocess(sentence):
+                vocab[word] += 1
+        return len(vocab)
+
+
 
 if __name__ == "__main__":
     english_data_path = "./training/hansards.36.2.e"
     french_data_path = "./training/hansards.36.2.f"
 
     data = DataLoader(english_data_path, french_data_path)
-    max_idx = 10
-    for idx, pair in enumerate(data.generate_sentence_pairs()):
-        if idx > max_idx:
-            break
-        print(pair)
+    # max_idx = 10
+    # for idx, pair in enumerate(data.generate_sentence_pairs()):
+    #     if idx > max_idx:
+    #         break
+    #     print(pair)
+    
