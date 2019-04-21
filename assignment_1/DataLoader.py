1751dc7784b838166831a404a87a467b52ec0a39
ximua00 <kishanarendra@gmail.com>
2019-04-21 14:08:40 +0200

    F to E, 0.3 validation score

--- assignment_1/DataLoader.py
+++ assignment_1/DataLoader.py
@@ -1,24 +1,24 @@
 from collections import defaultdict
 
 class DataLoader:
-    def __init__(self, english_data_path, french_data_path):
-        self.english_data_path = english_data_path
-        self.french_data_path = french_data_path
-        self.english_data = open(english_data_path, "r").readlines()
-        self.french_data = open(french_data_path, "r").readlines()
-        self.n_english_vocab =  self.vocabulary(self.english_data)
+    def __init__(self, source_data_path, target_data_path):
+        self.source_data_path = source_data_path
+        self.target_data_path = target_data_path
+        self.source_data = open(source_data_path, "r").readlines()
+        self.target_data = open(target_data_path, "r").readlines()
+        self.n_source_vocab =  self.vocabulary(self.source_data)
 
     def preprocess(self, sentence):
         output = sentence
-        output = output.lower()
+        # output = output.lower()
         return output.split()
     
     def generate_sentence_pairs(self):
-        for e,f in zip(self.english_data, self.french_data):
-            e = self.preprocess(e)
-            f = self.preprocess(f)
-            f.insert(0, "NULL")
-            yield (e,f)
+        for s,t in zip(self.source_data, self.target_data):
+            s = self.preprocess(s)
+            s.insert(0, "NULL") #add NULL to source language
+            t = self.preprocess(t)
+            yield (s,t)
 
     def vocabulary(self, languange_data):
         vocab = defaultdict(int)
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     french_data_path = "./training/hansards_test.36.2.f"
 
     data = DataLoader(english_data_path, french_data_path)
-    print(data.n_english_vocab)
+    print(data.n_source_vocab)
     # max_idx = 10
     # for idx, pair in enumerate(data.generate_sentence_pairs()):
     #     if idx > max_idx:
--- assignment_1/IBM1.py
+++ assignment_1/IBM1.py
@@ -11,13 +11,13 @@ PARAMETERS_PATH = "./models/IBM1/"
 PREDICTIONS_PATH = "./predictions/IBM1/"
 
 class IBM1:
-    def __init__(self, english_train_path, french_train_path, english_eval_path, french_eval_path):
-        self.training_data = DataLoader(english_train_path, french_train_path)
-        self.evaluation_data = DataLoader(english_eval_path, french_eval_path)
+    def __init__(self, source_train_path, target_train_path, source_eval_path, target_eval_path):
+        self.training_data = DataLoader(source_train_path, target_train_path)
+        self.evaluation_data = DataLoader(source_eval_path, target_eval_path)
         self.prob = self.uniform_initialisation()
     
     def uniform_initialisation(self):
-        unif_value = 1/self.training_data.n_english_vocab
+        unif_value = 1/self.training_data.n_source_vocab
         return defaultdict(lambda: defaultdict(lambda: unif_value))
 
     def train(self, num_iterations = 10):
@@ -30,21 +30,21 @@ class IBM1:
     def em_iteration(self):
         tcount = defaultdict(lambda: defaultdict(float))
         total = defaultdict(float)
-        for e_sentence,f_sentence in self.training_data.generate_sentence_pairs():    
-            for f_word in set(f_sentence):
+        for s_sentence, t_sentence in self.training_data.generate_sentence_pairs():    
+            for t_word in set(t_sentence):
                 denom_c = 0
-                for e_word in set(e_sentence):
-                    denom_c += self.prob[e_word][f_word] * f_sentence.count(f_word)
-                for e_word in set(e_sentence):
-                    weight = (self.prob[e_word][f_word] * f_sentence.count(f_word) * e_sentence.count(e_word)) / denom_c
-                    tcount[e_word][f_word] += weight                                                
-                    total[e_word] += weight
-        for e_word in tcount.keys():
-            for f_word in tcount[e_word].keys():
-                self.prob[e_word][f_word] = tcount[e_word][f_word] / total[e_word]
+                for s_word in set(s_sentence):
+                    denom_c += self.prob[s_word][t_word] * t_sentence.count(t_word)
+                for s_word in set(s_sentence):
+                    weight = (self.prob[s_word][t_word] * t_sentence.count(t_word) * s_sentence.count(s_word)) / denom_c
+                    tcount[s_word][t_word] += weight                                                
+                    total[s_word] += weight
+        for s_word in tcount.keys():
+            for t_word in tcount[s_word].keys():
+                self.prob[s_word][t_word] = tcount[s_word][t_word] / total[s_word]
 
     def save_checkpoint(self, iteration):
-        model_path = self.training_data.english_data_path.split("/")[2]
+        model_path = self.training_data.source_data_path.split("/")[2]
         with open(PARAMETERS_PATH + 'probs_{}_{}'.format(model_path, iteration) + '.pkl', 'wb') as f:
             dill.dump(self.prob, f)
 
@@ -56,10 +56,10 @@ class IBM1:
     
     def write_prediction(self, iteration = 9): 
         f = open(PREDICTIONS_PATH + "eval_prediction_{}.txt".format(iteration), "w+")
-        for sentence_idx, (e_sentence,f_sentence) in enumerate(self.evaluation_data.generate_sentence_pairs()):
-            alignments = self.viterbi_alignment(e_sentence, f_sentence)            
-            for e_align, f_align in alignments:
-                f.write("{} {} {} {} \n".format(sentence_idx+1, e_align+1, f_align, "S")) 
+        for sentence_idx, (s_sentence,t_sentence) in enumerate(self.evaluation_data.generate_sentence_pairs()):
+            alignments = self.viterbi_alignment(s_sentence, t_sentence)            
+            for s_align, t_align in alignments:
+                f.write("{} {} {} {} \n".format(sentence_idx+1, s_align+1, t_align, "S")) 
                 # TODO: wtf is S/P
         f.close()
 
@@ -68,17 +68,17 @@ class IBM1:
         print(aer)
         return aer
 
-    def viterbi_alignment(self, english_sentence, french_sentence):
+    def viterbi_alignment(self, source_sentence, target_sentence):
         sentence_alignment = []
-        for e_idx, e_word in enumerate(english_sentence):
-            max_f_idx = 0
-            max_f_prob = 0
-            for f_idx, f_word in enumerate(french_sentence):
-                probability = self.prob[e_word][f_word]
-                if probability > max_f_prob:
-                    max_f_prob = probability
-                    max_f_idx = f_idx
-            sentence_alignment.append((e_idx, max_f_idx))
+        for t_idx, t_word in enumerate(target_sentence):
+            max_s_idx = 0
+            max_s_prob = 0
+            for s_idx, s_word in enumerate(source_sentence):
+                probability = self.prob[s_word][t_word]
+                if probability > max_s_prob:
+                    max_s_prob = probability
+                    max_s_idx = s_idx
+            sentence_alignment.append((t_idx, max_s_idx)) #TODO: check this or switch
         return sentence_alignment
 
 
@@ -87,7 +87,10 @@ if __name__ == "__main__":
     french_train_path = "./training/hansards.36.2.f"
     english_eval_path  = "./validation/dev.e"
     french_eval_path = "./validation/dev.f"
-    ibm1 = IBM1(english_train_path, french_train_path, english_eval_path, french_eval_path)
+    ibm1 = IBM1(source_train_path = french_train_path,
+                target_train_path= english_train_path,
+                source_eval_path= french_eval_path,
+                target_eval_path = english_eval_path)
     ibm1.train()
     # ibm1.load_checkpoint()
     # ibm1.write_prediction(9)
