abd9a0c4785c86921af123de30e2abad5b60a5af
ximua00 <kishanarendra@gmail.com>
2019-04-22 14:50:27 +0200

    enabled evaluation on test set, current 0.28AER

--- assignment_1/IBM1.py
+++ assignment_1/IBM1.py
@@ -11,13 +11,15 @@ from aer import test
 
 PARAMETERS_PATH = "./models/IBM1/"
 PREDICTIONS_PATH = "./predictions/IBM1/"
+TEST_PATH = "./testing/eval/"
 PLOTS_PATH = "./plots/IBM1/"
-EXPERIMENT_NAME = "lowercase"
+EXPERIMENT_NAME = "baseline"
 
 class IBM1:
-    def __init__(self, source_train_path, target_train_path, source_eval_path, target_eval_path):
+    def __init__(self, source_train_path, target_train_path, source_eval_path, target_eval_path, source_test_path, target_test_path):
         self.training_data = DataLoader(source_train_path, target_train_path)
         self.evaluation_data = DataLoader(source_eval_path, target_eval_path)
+        self.test_data = DataLoader(source_test_path, target_test_path)
         self.prob = self.uniform_initialisation()
     
     def uniform_initialisation(self):
@@ -30,7 +32,7 @@ class IBM1:
         for iteration in range(num_iterations):
             self.em_iteration()
             self.save_checkpoint(iteration)
-            self.write_prediction(iteration)
+            self.write_prediction(data = self.evaluation_data, iteration = iteration)
             aer, log_likelihood = self.evaluate(iteration)
             aers.append(aer)
             log_likelihoods.append(log_likelihood)
@@ -61,25 +63,32 @@ class IBM1:
         all_checkpoints = os.listdir(PARAMETERS_PATH)
         latest_checkpoint = sorted(all_checkpoints)[-1]
         with open(PARAMETERS_PATH + latest_checkpoint, 'rb') as f:
+            print("loading", PARAMETERS_PATH + latest_checkpoint)
             self.prob = dill.load(f)
     
-    def write_prediction(self, iteration = 9): 
-        f = open(PREDICTIONS_PATH + "eval_prediction_{}_{}.txt".format(EXPERIMENT_NAME, iteration), "w+")
-        for sentence_idx, (s_sentence,t_sentence) in enumerate(self.evaluation_data.generate_sentence_pairs()):
+    def write_prediction(self, data, path = PREDICTIONS_PATH, output_type = "eval", iteration = 9): 
+        f = open(path + "prediction_{}_{}.txt".format(EXPERIMENT_NAME, iteration), "w+")
+        for sentence_idx, (s_sentence,t_sentence) in enumerate(data.generate_sentence_pairs()):
             alignments = self.viterbi_alignment(s_sentence, t_sentence)            
             for s_align, t_align in alignments:
-                f.write("{} {} {} {} \n".format(sentence_idx+1, t_align+1, s_align, "S")) 
-                # TODO: wtf is S/P
+                if output_type == "eval":
+                    f.write("{} {} {} {} \n".format(sentence_idx+1, t_align+1, s_align, "S")) 
+                if output_type == "test":
+                    f.write("{} {} {} {} \n".format(self.add_back_zeros(sentence_idx+1), t_align+1, s_align, "S"))
         f.close()
 
     def evaluate(self, iteration):
         aer = test("./validation/dev.wa.nonullalign", PREDICTIONS_PATH + "eval_prediction_{}_{}.txt".format(EXPERIMENT_NAME, iteration))
         log_likelihood = 0
-        for sentence_idx, (s_sentence,t_sentence) in enumerate(self.evaluation_data.generate_sentence_pairs()):
+        for s_sentence,t_sentence in self.training_data.generate_sentence_pairs():
             log_likelihood += self.calculate_log_likelihood(s_sentence, t_sentence)
         print("AER:", aer)
         print("Log-Likelihood:", log_likelihood)
         return aer, log_likelihood
+
+    def test(self):
+        # write predictions for test set
+        self.write_prediction(data = self.test_data, path = TEST_PATH, output_type = "test")
 
     def viterbi_alignment(self, source_sentence, target_sentence):
         sentence_alignment = []
@@ -101,6 +110,13 @@ class IBM1:
             log_likelihood += log(self.prob[source_sentence[s_idx]][target_sentence[t_idx]])
         return log_likelihood
 
+    @staticmethod
+    def add_back_zeros(number, number_of_back_zeros=4):
+        number = str(number)
+        while len(number) < number_of_back_zeros:
+            number = "0" + number
+        return number
+
     def plot_results(self, aers, log_likelihoods):
         plt.plot(aers, label = "AER")
         plt.legend()
@@ -116,11 +132,14 @@ if __name__ == "__main__":
     french_train_path = "./training/hansards.36.2.f"
     english_eval_path  = "./validation/dev.e"
     french_eval_path = "./validation/dev.f"
+    english_test_path = "./testing/test/test.e"
+    french_test_path = "./testing/test/test.f"
     ibm1 = IBM1(source_train_path = french_train_path,
                 target_train_path= english_train_path,
                 source_eval_path= french_eval_path,
-                target_eval_path = english_eval_path)
-    ibm1.train()
-    # ibm1.load_checkpoint()
-    # ibm1.write_prediction(9)
-    # ibm1.evaluate(9)
+                target_eval_path = english_eval_path,
+                source_test_path = french_test_path,
+                target_test_path = english_test_path)
+    # ibm1.train()
+    ibm1.load_checkpoint()
+    ibm1.test()
